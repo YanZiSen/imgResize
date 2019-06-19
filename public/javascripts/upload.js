fd093ee0ef913277283b94001756f29bda0a0744
yanning <yanningning@jiuqi.com.cn>
2019-06-19 19:42:35 +0800

    展示与编辑完成

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -6,6 +6,61 @@ uploadImage.onchange = function (e) {
       'Content-Type': 'multipart/form-data'
     }
   }).then(res => {
-    console.log(res)
+    console.log(res.data.fileUrl)
+    drawImage(res.data.fileUrl)
   })
 }
+
+function ImageOnLoad (src) {
+  var image = new Image()
+  image.src = src
+  return new Promise((resolve, reject) => {
+    image.onload = function () {
+      resolve(image)
+    }
+    image.onerror = function (e) {
+      reject(e)
+    }
+  })
+}
+
+async function drawImage (src) {
+  var stage = new Konva.Stage({
+    container: document.querySelector('.img-box'),
+    width: 500,
+    height: 500
+  })
+  var layer = new Konva.Layer()
+  let image = await ImageOnLoad(src)
+  var imageCanvas = new Konva.Image({
+    image: image,
+    x: 50,
+    y: 50,
+    width: 120,
+    height: 120,
+    draggable: true
+  })
+  layer.add(imageCanvas)
+  stage.add(layer)
+  stage.on('click tap', function (e) {
+    if (e.target === stage) {
+      stage.find('Transformer').destroy()
+      layer.draw();
+      return
+    }
+    if (!e.target.hasName('img')) {
+      return
+    }
+    stage.find('Transformer').destroy();
+    var tr = new Konva.Transformer();
+    layer.add(tr)
+    tr.attachTo(e.target)
+    layer.draw();
+  })
+}
+
+// class DrawImage {
+//   constructor (src) {
+//
+//   }
+// }
--- routes/index.js
+++ routes/index.js
@@ -12,33 +12,38 @@ router.get('/', function(req, res, next) {
 router.post('/uploadFile', function(req, res, next){
 	var form = new formidable.IncomingForm();
 	form.uploadDir = path.join(__dirname, '../public/images')
-	form.keepExtension = true
-	console.log('aaaaa')
+	form.keepExtensions = true
 	form.parse(req, function (err, fields, files){
-		console.log('files', files)
+		// console.log('files', files)
 		let filePath = '';
 		if (files.tmpFile) {
 			filePath = file.tmpFile.path;
 		} else {
 			for (var key in files) {
 				if (files[key].path && filePath === '') {
-					filePath = file[key].path;
+					filePath = files[key].path;
 					break; // 取第一个文件路径
 				}
 			}
 		}
 		// 文件移动
-		var targetDir = path.join(__dirname, 'upload');
+		var targetDir = path.join(__dirname, '../public/upload');
 		if (!fs.existsSync(targetDir)) {
 			fs.mkdir(targetDir);
 		}
+		console.log(filePath)
 		// 获取文件后缀
 		var fileExt = filePath.substring(filePath.lastIndexOf('.'));
 		// 判断文件类型是否允许上传
 		if (('.jpg.jpeg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
 			var err = new Error('文件类型不允许上传');
+			res.json({
+				code: -1,
+				message: '此文件类型不允许上传'
+			});
 		} else {
 			var fileName = new Date().getTime() + fileExt;
+			console.log('fileName', fileName);
 			var targetFile = path.join(targetDir, fileName);
 			// 移动文件
 			fs.rename(filePath, targetFile, function (err) {
@@ -46,7 +51,7 @@ router.post('/uploadFile', function(req, res, next){
 					console.log(err);
 					res.json({code: -1, message: '操作失败'});
 				} else {
-					var fileUrl = '/upload' + fileName;
+					var fileUrl = '/upload/' + fileName;
 					res.json({
 						code: 0,
 						fileUrl: fileUrl
